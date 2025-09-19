ea78d57f9b40d94076420a2c13fd53f811b4ddc2
Nathan Wilson <natewilson869@gmail.com>
2025-09-19 14:28:47 -0400

    added validity checks and finished dropping value function

--- grades.c
+++ grades.c
@@ -2,7 +2,17 @@
 /* assign info has calculated
  * value and assignment number
  */
-#define assign_info 4
+
+/* to make sure weights add up to 100 */
+int check_valid(int num_assign, int weights[]){
+	int i, weight_sum = 0;
+	for(i = 0; i < num_assign; i++) {
+		weight_sum += weights[i];
+	}
+	/* returns 1 if true */
+	return weight_sum == 100;
+}
+
 
 double calculate_assign_val(int score, int weight) {
 
@@ -15,13 +25,13 @@ double calculate_assign_val(int score, int weight) {
  * array, the needed array size and 
  * num of assignments
  */
-double find_lowest_value(double scores[], int array_size, int assign_nums[]) {
+double drop_lowest_value(double scores[], int array_size, int assign_nums[]) {
 
     int i;
     double min_value = 100;
     int index_of_min = 0;
 
-    for (i = 0; i < array_size; i++) {
+    for (i = 0; i <= array_size; i++) {
         /* getting the min value  */ 
         if (scores[i] < min_value) {
             min_value = scores[i];
@@ -36,6 +46,14 @@ double find_lowest_value(double scores[], int array_size, int assign_nums[]) {
                 index_of_min = i;
             }
         }
+    }
+
+    /* resizing array by shifting elements from
+     * point of min left
+     */
+    for(i = index_of_min; i < array_size -1; i++){
+	    scores[i] = scores[i + 1];
+	    assign_nums[i] = assign_nums[i + 1];
     }
     return min_value;
 }
@@ -52,7 +70,7 @@ int main() {
     scanf("%d", &num_assign);
 
     int assign_nums[num_assign], weight_array[num_assign], late_array[num_assign];
-    double numeric_scores[num_assign], dropped_value;
+    double numeric_scores[num_assign], dropped_val, average, score_sum = 0.0;
 
     for (i = 0; i < num_assign; i++) {
         scanf("%d, %d, %d, %d", &assign_num, &score, &weight, &days_late);
@@ -70,7 +88,35 @@ int main() {
         late_array[i] = days_late;
     }
 
-    dropped_value = find_lowest_value(numeric_scores, num_assign, assign_nums);
+    /* make sure weights add up */
+    if( check_valid(num_assign, weight_array) == 0 ) {
 
-    printf("the lowest value is: %f\n", dropped_value); 
+
+    /* creating var so num_assign is 
+     * not changed in the following method */
+    int assign_after_drop = num_assign;
+
+    /* summing numeric scores */
+    for(i=0; i< num_assign; i++) {
+        score_sum += numeric_scores[i];
+    }
+
+    /* adjusting sum after dropping the lowest values */ 
+    for(i=0; i < num_to_drop; i++) {
+	dropped_val = drop_lowest_value(numeric_scores, assign_after_drop, assign_nums);
+    	assign_after_drop--;
+	score_sum -= dropped_val;
+
+
+    }
+
+
+    average = score_sum / assign_after_drop;
+
+    printf("the lowest value is: %f\n", dropped_val); 
+
+    /* if weights don't add up */
+    } else {
+	    printf("ERROR: Invalid values provided");
+    }
 }
